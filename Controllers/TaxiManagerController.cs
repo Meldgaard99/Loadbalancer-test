729fb632233890dc0b6e4f7e42e2dc258fd53b7e
Meldgaard99 <100026274+Meldgaard99@users.noreply.github.com>
2023-11-08 19:01:56 +0100

    error in using taximanagerapi.models

--- Controllers/TaxiManagerController.cs
+++ Controllers/TaxiManagerController.cs
@@ -1,34 +1,60 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-namespace TaxiManagerAPI.Controllers;
+using System.Collections.Generic;
+using TaxiManagerAPI.Models;
 
 
-[ApiController]
-[Route("[controller]")]
-public class TaxiManagerController : ControllerBase
+
+
+namespace TaxiManagerAPI.Controllers
 {
-    
-    private readonly ILogger<TaxiManagerController> _logger;
-
-    public TaxiManagerController(ILogger<TaxiManagerController> logger)
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxiManagerController : ControllerBase
     {
-        _logger = logger;
-    }
+        private readonly ILogger<TaxiManagerController> _logger;
 
-    [HttpGet("version")]
-    public async Task<Dictionary<string, string>> GetVersion()
+        public TaxiManagerController(ILogger<TaxiManagerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("version")]
+        public async Task<Dictionary<string, string>> GetVersion()
+        {
+            var properties = new Dictionary<string, string>();
+            var assembly = typeof(Program).Assembly;
+            properties.Add("service", "TaxiManager");
+            var ver = FileVersionInfo.GetVersionInfo(
+            typeof(Program).Assembly.Location).ProductVersion ?? "N/A";
+            properties.Add("version", ver);
+            var hostName = System.Net.Dns.GetHostName();
+            var ips = await System.Net.Dns.GetHostAddressesAsync(hostName);
+            var ipa = ips.First().MapToIPv4().ToString() ?? "N/A";
+            properties.Add("ip-address", ipa);
+            return properties;
+        }
+
+
+
+       [HttpGet("cars")]
+public IEnumerable<Car> GetCars()
+{
+    var cars = new List<Car>
     {
-        var properties = new Dictionary<string, string>();
-        var assembly = typeof(Program).Assembly;
-        properties.Add("service", "TaxiManager");
-        var ver = FileVersionInfo.GetVersionInfo(
-        typeof(Program).Assembly.Location).ProductVersion ?? "N/A";
-        properties.Add("version", ver);
-        var hostName = System.Net.Dns.GetHostName();
-        var ips = await System.Net.Dns.GetHostAddressesAsync(hostName);
-        var ipa = ips.First().MapToIPv4().ToString() ?? "N/A";
-        properties.Add("ip-address", ipa);
-        return properties;
+        new Car
+        {
+            Make = "Toyota",
+            Model = "Corolla",
+            Year = 2018,
+            Color = "Red",
+            Mileage = 50000
+        }
+
+    };
+
+    return cars;
+}
     }
 
 }
